e107b118eb5caad4d03992acc1ccf1783eaa35a4
lovepend <lovepend@nate.com>
2023-05-31 22:38:38 +0900

    육부촌

--- Camp_51_육부촌.py
+++ Camp_51_육부촌.py
@@ -20,29 +20,29 @@ pag.FAILSAFE=False
 
 #★★★★★★★★★★★★★★★★★★★ 날짜 입력
 def 육부촌_Day1():
-    global 육부촌_day1
-    육부촌_day1 = 945,379
-    print(육부촌_day1)
+    global 육부촌_day
+    육부촌_day = 945,379
+    print(육부촌_day)
 
 def 육부촌_Day2():
-    global 육부촌_day2
-    육부촌_day2= 945,402
-    print(육부촌_day2)
+    global 육부촌_day
+    육부촌_day= 945,402
+    print(육부촌_day)
 
 def 육부촌_Day3():
-    global 육부촌_day3
-    육부촌_day3 = 945,419
-    print(육부촌_day3)
+    global 육부촌_day
+    육부촌_day = 945,419
+    print(육부촌_day)
 
 def 육부촌_Day4():
-    global 육부촌_day4
-    육부촌_day4 = 945,439
-    print(육부촌_day4)
+    global 육부촌_day
+    육부촌_day = 945,439
+    print(육부촌_day)
 
 def 육부촌_Day5():
-    global 육부촌_day5
-    육부촌_day5 = 945,461
-    print(육부촌_day5)
+    global 육부촌_day
+    육부촌_day = 945,461
+    print(육부촌_day)
 
 ################################## 범위 
 global 육부촌_find_range
@@ -55,26 +55,26 @@ global 육부촌_position_down
 육부촌_position_down  = 1300,909
 
 ################################## 날짜
-global 육부촌_day1
+global 육부촌_day
 #육부촌_day1 = 945,379 #첫주
-육부촌_day1 = 900, 402 # 21일
-global 육부촌_day2
-육부촌_day2 = 900, 402 # 22일
-global 육부촌_day3
-육부촌_day3 = 900, 402 # 23일
-global 육부촌_day4
-육부촌_day4 = 900, 421 #
-global 육부촌_day5
-육부촌_day5 = 25,49 #뒤로
+육부촌_day = 900, 402 # 21일
+# global 육부촌_day2
+# 육부촌_day2 = 900, 402 # 22일
+# global 육부촌_day3
+# 육부촌_day3 = 900, 402 # 23일
+# global 육부촌_day4
+# 육부촌_day4 = 900, 421 #
+# global 육부촌_day5
+# 육부촌_day5 = 25,49 #뒤로
 #육부촌_day5 = 709, 442 #23일
 #육부촌_day5 = 902,441
 #★★★★★★★★★★★★★★★★★★★ 날짜 입력
 def 육부촌_Day():
     while True:
         if keyboard.is_pressed("F2"): 
-            global 육부촌_day1 
-            육부촌_day1  = pag.position()
-            print(육부촌_day1)
+            global 육부촌_day
+            육부촌_day  = pag.position()
+            print(육부촌_day)
             time.sleep(1)
             break         
 
@@ -124,21 +124,19 @@ i = 0
 #★★★★★★★★★★★★★★★★★★★ 메인 함수 
 def 육부촌_Start():
     while True:
-        global 육부촌_day1
-        global 육부촌_day2
-        global 육부촌_day3
-        global 육부촌_day4
-        global 육부촌_day5
+        global 육부촌_day
+      
         global i
         i = i +1
         print(i)
         if keyboard.is_pressed("F2") : # F2 누른게 감지되면
             print("F2 종료")
             break
+        pag.hotkey('f5')
         #날짜 클릭
-        time.sleep(0.5)
-        pag.click(육부촌_day1)
-        time.sleep(1)
+        #time.sleep(0.5)
+        pag.click(육부촌_day)
+        #time.sleep(1)
         육부촌_검색()
         #pag.click(육부촌_day2)
         #time.sleep(1)
@@ -149,8 +147,8 @@ def 육부촌_Start():
         #pag.click(육부촌_day4)
         #time.sleep(1)
         #육부촌_검색()
-        pag.click(육부촌_day5)
-        time.sleep(1)
+        #pag.click(육부촌_day5)
+        #time.sleep(1)
         육부촌_검색()
 
 
@@ -165,45 +163,52 @@ def 육부촌_검색():
         육부촌_확인()
         
 def 육부촌_확인() :
-    time.sleep(0.5)
+    time.sleep(0.2)
     button1 = pag.locateCenterOnScreen('./image/51. Kyungju/05._2. next.png', confidence=0.7)   
     print(button1)
     if (button1 == None) :
         육부촌_확인()      
     else :
         pag.click(button1.x,button1.y, button='left', clicks=5, interval=0.1)
-        refresh_token()
-        kakao_message_you("육부촌")
+        #refresh_token()
+        #kakao_message_you("육부촌")
         육부촌_주소()
 
 def 육부촌_주소():
-    time.sleep(0.5)
+    time.sleep(0.2)
     #주소검색
-    pag.click(837,699)
+    button = pag.locateCenterOnScreen('./image/51. Kyungju/05._4. Adress.png', confidence=0.7)   
+    pag.click(button.x,button.y, button='left', clicks=1, interval=0.1) 
     pyperclip.copy('반구로55')
     time.sleep(0.2)
     #예)판교역로235
-    pag.click(315,139)
-    time.sleep(0.4)
+    button = pag.locateCenterOnScreen('./image/51. Kyungju/05._5. Adress.png', confidence=0.7)   
+    pag.click(button.x,button.y, button='left', clicks=1, interval=0.1) 
+    #pag.click(197,135)
+    time.sleep(0.2)
     #반구로 55 붙이기
     pag.hotkey('ctrl','v')
-    time.sleep(0.4)
+    time.sleep(0.2)
     #입력
     pag.hotkey('enter')
-    time.sleep(0.4)
+    time.sleep(0.2)
     #주소클릭
-    pag.click(295,263)   
-    time.sleep(0.4)
+    button = pag.locateCenterOnScreen('./image/51. Kyungju/05._6. Adress.png', confidence=0.7)   
+    pag.click(button.x,button.y, button='left', clicks=1, interval=0.1) 
+    #pag.click(295,263)   
+    time.sleep(0.2)
     #기본주소 클릭
-    pag.click(862,746) 
-    pyperclip.copy('107동 1001호')
-    pag.hotkey('ctrl','v')
-    time.sleep(0.4)
+    #pag.click(862,746) 
+    #pyperclip.copy('107동 1001호')
+    #pag.hotkey('ctrl','v')
+    #time.sleep(0.4)
+    #자동입력방지
+    pag.click(982,797)
     #예약신청
-    pag.click(1021,939)
-    button2 = pag.locateCenterOnScreen('./image/51. Kyungju/05._3. next.png', confidence=0.7)   
-    pag.click(button2.x,button2.y, button='left', clicks=1, interval=0.1) 
-    time.sleep(0.4)
+    #pag.click(1021,939)
+    #button2 = pag.locateCenterOnScreen('./image/51. Kyungju/05._3. next.png', confidence=0.7)   
+    #pag.click(button2.x,button2.y, button='left', clicks=1, interval=0.1) 
+    #time.sleep(0.4)
     time.sleep(500)
 
 
